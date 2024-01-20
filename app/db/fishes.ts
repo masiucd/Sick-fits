8a1c27f7f7f57846b4b3dba32a012ae50bf43bf9
masiucd <ciszekmarcell@gmail.com>
2024-01-20 15:31:46 +0100

    Add date-fns library and update package.json and pnpm-lock.yaml

--- app/db/fishes.ts
+++ app/db/fishes.ts
@@ -6,8 +6,10 @@ export const FishSchema = z.object({
   name: z.string(),
   species: z.string(),
   description: z.string(),
-  image: z.string(),
+  price: z.number(),
   state: z.string(), // fresh or soldOut
+  created_at: z.string(),
+  image: z.string(),
 });
 
 export type Fish = z.infer<typeof FishSchema>;
@@ -18,6 +20,22 @@ export const fishes = sqliteTable("fishes", {
   name: text("name"),
   species: text("species"),
   description: text("description"),
-  image: text("image"),
+  price: integer("price"),
   state: text("state"),
+  created_at: text("created_at"),
+});
+
+export const FishImageSchema = z.object({
+  id: z.number(),
+  image: z.string(),
+});
+
+export type FishImage = z.infer<typeof FishImageSchema>;
+
+export const FishImagesSchema = z.array(FishImageSchema);
+
+export const fishImages = sqliteTable("fish_images", {
+  id: integer("id"),
+  fish_id: integer("fish_id"),
+  image: text("image"),
 });
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,14 @@
-import type {
-  ActionFunction,
-  ActionFunctionArgs,
-  LoaderFunctionArgs,
-  MetaFunction,
+import {
+  redirect,
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+  type MetaFunction,
 } from "@remix-run/node";
-import {useFetcher} from "@remix-run/react";
+import {useFetcher, useLoaderData} from "@remix-run/react";
 import {db} from "~/db/db";
-import {FishesSchema, fishes} from "~/db/fishes";
+import {FishImagesSchema, FishesSchema, fishImages, fishes} from "~/db/fishes";
+import {format, parseISO} from "date-fns";
+import {asc, desc, eq} from "drizzle-orm";
 
 export const meta: MetaFunction = () => {
   return [
@@ -17,17 +19,20 @@ export const meta: MetaFunction = () => {
 
 export async function action({request}: ActionFunctionArgs) {
   const formData = await request.formData();
+
   const name = formData.get("name");
   const species = formData.get("species");
   const description = formData.get("description");
   const image = formData.get("image");
   const state = formData.get("state");
+  const price = formData.get("price");
   if (
     typeof name !== "string" ||
     typeof species !== "string" ||
     typeof description !== "string" ||
     typeof image !== "string" ||
-    typeof state !== "string"
+    typeof state !== "string" ||
+    typeof price !== "string"
   ) {
     return new Response("Invalid form data", {status: 400});
   }
@@ -36,36 +41,90 @@ export async function action({request}: ActionFunctionArgs) {
     name,
     species,
     description,
-    image,
     state,
+    price: parseFloat(price),
+    created_at: new Date().toISOString(),
   };
-  console.log("newFish", newFish);
-  await db.insert(fishes).values(newFish);
-  return null;
+
+  const [{id}] = await db
+    .insert(fishes)
+    .values(newFish)
+    .returning({id: fishes.id});
+  if (id) {
+    await db.insert(fishImages).values({image, fish_id: id});
+  }
+
+  return redirect("/", {
+    headers: {
+      // "Set-Cookie": `fishId=${name}; Max-Age=60; HttpOnly; Path=/`,
+      "Cache-Control": "no-cache",
+    },
+  });
 }
 
 export async function loader({request, params}: LoaderFunctionArgs) {
-  const rows = await db
+  const fishItems = await db
     .select({
       id: fishes.id,
       name: fishes.name,
       species: fishes.species,
       description: fishes.description,
-      image: fishes.image,
+      price: fishes.price,
       state: fishes.state,
+      created_at: fishes.created_at,
+      image: fishImages.image,
     })
-    .from(fishes);
-  return FishesSchema.parse(rows);
+    .from(fishes)
+    .innerJoin(fishImages, eq(fishes.id, fishImages.fish_id))
+    .orderBy(desc(fishes.created_at));
+
+  const images = await db
+    .select({
+      id: fishImages.id,
+      image: fishImages.image,
+    })
+    .from(fishImages);
+
+  const fishesItemsList = FishesSchema.parse(fishItems);
+  const fishImagesList = FishImagesSchema.parse(images);
+
+  return {
+    fishes: fishesItemsList,
+    fishImages: fishImagesList,
+  };
 }
 
 export default function Index() {
-  // const fishes = useLoaderData<typeof loader>();
+  const {fishes, fishImages} = useLoaderData<typeof loader>();
   const fetcher = useFetcher();
   return (
     <section className="my-10 flex flex-1 flex-col">
       <div className="grid flex-1  grid-cols-1 md:grid-cols-12">
         <div className="col-span-4 border-2 border-gray-900">
           <h1>Welcome to catch of today</h1>
+          <ul>
+            {fishes.map((fish) => {
+              return (
+                <li key={fish.id}>
+                  <h2>{fish.name}</h2>
+                  <h2>
+                    {fish.price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </h2>
+                  <p>{fish.species}</p>
+                  <p>{fish.description}</p>
+                  <p>{format(parseISO(fish.created_at), "MMMM do, yyyy")}</p>
+
+                  <img
+                    src={`/images/${fish.image || fish.name}.jpg`}
+                    alt={fish.name}
+                  />
+                </li>
+              );
+            })}
+          </ul>
         </div>
         <div className="col-span-3 border-2 border-gray-900 ">
           <h2 className="mb-5">Your order</h2>
@@ -81,13 +140,13 @@ export default function Index() {
           <h3>Inventory</h3>
           <fetcher.Form method="post">
             <fieldset className="flex flex-col gap-2">
-              <div className="flex">
-                <div className="flex flex-col">
+              <div className="flex flex-col border-2 md:flex-row">
+                <div className="flex flex-1 flex-col">
                   <label htmlFor="name">Name</label>
                   <input type="text" id="name" name="name" required />
                 </div>
 
-                <div className="flex flex-col">
+                <div className="flex flex-1 flex-col">
                   <label htmlFor="species">Species</label>
                   <select name="species" id="species" required>
                     <option defaultChecked value="shell">
@@ -98,8 +157,10 @@ export default function Index() {
                     <option value="lobster">Lobster</option>
                   </select>
                 </div>
+              </div>
 
-                <div className="flex gap-5">
+              <div className="flex flex-col border-2 md:flex-row">
+                <div className="flex flex-1 gap-5">
                   <div>
                     <label htmlFor="fresh">Fresh</label>
                     <input
@@ -121,6 +182,11 @@ export default function Index() {
                     />
                   </div>
                 </div>
+
+                <div className="flex flex-1 flex-col gap-1">
+                  <label htmlFor="price">Price</label>
+                  <input type="number" name="price" id="price" />
+                </div>
               </div>
 
               <div className="flex flex-col">
@@ -131,7 +197,14 @@ export default function Index() {
 
                 <div className="flex flex-col gap-1">
                   <label htmlFor="image">Image</label>
-                  <input type="text" id="image" name="image" />
+
+                  <select name="image" id="image">
+                    {fishImages.map((fish) => (
+                      <option key={fish.id} value={fish.image}>
+                        {fish.image}
+                      </option>
+                    ))}
+                  </select>
                 </div>
               </div>
               <button
@@ -144,23 +217,6 @@ export default function Index() {
           </fetcher.Form>
         </div>
       </div>
-
-      {/* <ul className="grid grid-cols-3 gap-4">
-        {fishes.map((fish) => {
-          console.log("fish.image", fish.image);
-          return (
-            <li key={fish.id}>
-              <h2>{fish.name}</h2>
-              <p>{fish.species}</p>
-              <p>{fish.description}</p>
-              <img
-                src={`/images/${fish.image || fish.name}.jpg`}
-                alt={fish.name}
-              />
-            </li>
-          );
-        })}
-      </ul> */}
     </section>
   );
 }
