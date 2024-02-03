674b53b70b0c2aa88be0e87d3437846666044c77
masiucd <ciszekmarcell@gmail.com>
2024-02-03 13:55:01 +0100

    Delete unused code and optimize database queries

--- app/db/orders/dao.server.ts
+++ app/db/orders/dao.server.ts
@@ -1,6 +1,4 @@
-import {desc, eq} from "drizzle-orm";
 import {db} from "../db.server";
-import {SeaCatches} from "../records/sea-catches.server";
 import {Orders} from "../records/orders.server";
 
 export async function insertIntoOrder(seaCathId: number) {
@@ -11,22 +9,4 @@ export async function insertIntoOrder(seaCathId: number) {
       created_at: new Date().toISOString(),
     })
     .returning({id: Orders.id});
-}
-
-export async function deleteOrder(cartItemId: number) {
-  await db.delete(Orders).where(eq(Orders.id, cartItemId));
-}
-
-export async function getOrderItems() {
-  return await db
-    .select({
-      id: Orders.id,
-      catch_id: Orders.catch_id,
-      name: SeaCatches.name,
-      price: SeaCatches.price,
-      species: SeaCatches.species,
-      description: SeaCatches.description,
-    })
-    .from(Orders)
-    .innerJoin(SeaCatches, eq(Orders.catch_id, SeaCatches.id));
 }
